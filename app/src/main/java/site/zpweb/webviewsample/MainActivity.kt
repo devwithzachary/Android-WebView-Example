e3cb059e104a6bd555ac2fdd291bf1058604046b
Zachary Powell <zachary.powell@vonage.com>
2022-09-06 09:04:16 +0100

    Display hello world html in webview

--- app/src/main/java/site/zpweb/webviewsample/MainActivity.kt
+++ app/src/main/java/site/zpweb/webviewsample/MainActivity.kt
@@ -2,10 +2,15 @@ package site.zpweb.webviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var webView = findViewById<WebView>(R.id.webview)
+        webView.loadUrl("file:///android_asset/test.html");
+
     }
 }
